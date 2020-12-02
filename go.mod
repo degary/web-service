b3d7c3c5f67a535ef5aa14a710e7a887d42ac6c2
degary <degary@qq.com>
2020-12-02 21:26:30 +0800

    增加参数验证

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-openapi/spec v0.19.15 // indirect
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/jinzhu/gorm v1.9.12
 	github.com/json-iterator/go v1.1.10 // indirect
@@ -29,7 +31,7 @@ require (
 	github.com/ugorji/go v1.2.0 // indirect
 	github.com/urfave/cli v1.22.5 // indirect
 	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
-	golang.org/x/sys v0.0.0-20201130171929-760e229fe7c5 // indirect
+	golang.org/x/sys v0.0.0-20201201145000-ef89a241ccb3 // indirect
 	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/tools v0.0.0-20201201064407-fd09bd90d85c // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
--- internal/routers/api/v1/tag.go
+++ internal/routers/api/v1/tag.go
@@ -1,12 +1,18 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/degary/web-service/global"
+	"github.com/degary/web-service/pkg/app"
+	"github.com/degary/web-service/pkg/errcode"
+	"github.com/gin-gonic/gin"
+)
 
-type Tag struct {}
+type Tag struct{}
 
 func NewTag() Tag {
 	return Tag{}
 }
+
 // @Summary 获取多个标签
 // @Produce  json
 // @Param name query string false "标签名称" maxlength(100)
@@ -29,7 +35,22 @@ func NewTag() Tag {
 // @Failure 400 {object} errcode.Error "请求错误"
 // @Failure 500 {object} errcode.Error "内部错误"
 // @Router /api/v1/tags [get]
-func (t Tag) List(c *gin.Context)   {}
+func (t Tag) List(c *gin.Context) {
+	param := struct {
+		Name  string `form:"name" binding:"max=10"`
+		State uint8  `form:"state,default=1" binding:"oneof=0 1"`
+	}{}
+	response := app.NewResponse(c)
+	valid, errs := app.BindAndValid(c, &param)
+	if valid == true {
+		global.Logger.Errorf("app.BindAndValid errs: %v", errs)
+		response.ToErrorResponse(errcode.InvalidParams.WithDetails(errs.Errors()...))
+		return
+	}
+	response.ToResponse(gin.H{})
+	return
+}
+
 // @Summary 新增标签
 // @Produce  json
 // @Param name body string true "标签名称" minlength(3) maxlength(100)
@@ -40,6 +61,7 @@ func (t Tag) List(c *gin.Context)   {}
 // @Failure 500 {object} errcode.Error "内部错误"
 // @Router /api/v1/tags [post]
 func (t Tag) Create(c *gin.Context) {}
+
 // @Summary 更新标签
 // @Produce  json
 // @Param id path int true "标签ID"
